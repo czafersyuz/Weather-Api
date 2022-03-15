2266ff8006131d683f20ff39d9f874b841686bd3
czafersyuz <94002068+czafersyuz@users.noreply.github.com>
2022-03-15 16:00:06 +0300

    weather api

---
+++ weather.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector("div.container input");
+const msg = document.querySelector("span.msg");
+const cityList = document.querySelector(".ajax-section .cities");
+
+ 
+
+
+
+
+
+
+
+
