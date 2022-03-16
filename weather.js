ddd958708d660caf44d7b1162e9c501a19893061
czafersyuz <94002068+czafersyuz@users.noreply.github.com>
2022-03-16 15:32:15 +0300

    finish

--- weather.js
+++ weather.js
@@ -3,14 +3,14 @@ const input = document.querySelector("div.container input");
 const msg = document.querySelector("span.msg");
 const cityList = document.querySelector(".ajax-section .cities");
 
- localStorage.setItem("apiKey", EncryptStringAES ("c71e40ff2e4979ebd368926f2493bba8"));
- 
- form.addEventListener("submit", (event)=>{
-     event.preventDefault();
-     getWeatherDataFromApi();
- });
+localStorage.setItem("apiKey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
 
- const getWeatherDataFromApi = async() =>{
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    getWeatherDataFromApi();
+});
+
+const getWeatherDataFromApi = async() =>{
     let apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
     let inputVal = input.value;
     let units = "metric";
@@ -20,22 +20,56 @@ const cityList = document.querySelector(".ajax-section .cities");
 
     try {
         const response = await axios(url);
-        console.log(response);
+        // console.log(response);
         const { main, name, sys, weather} = response.data;
-        //console.log(weather[0].icon);
-        //img url
-        const iconUrl = `const iconUrl = `https://openweathermap.org/img/wn/${
-            weather[0].icon}@2x.png`;`
-            
+        // console.log(weather[0].icon);
+        //image url
+        const iconUrl = `https://openweathermap.org/img/wn/${
+            weather[0].icon}@2x.png`;
+        console.log(response);
+
+        let cityCardList = cityList.querySelectorAll(".city");
+        let cityCardListArray = Array.from(cityCardList);
+        if(cityCardListArray.length > 0){
+            const filteredArray = cityCardListArray.filter(card => card.querySelector(".city-name span").innerText == name);
+            if(filteredArray.length > 0){
+                msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+                setTimeout(()=>{
+                    msg.innerText = "";
+                }, 5000);
+                form.reset();
+                input.focus();
+                return;
+            }
+        }
         
+        let createdCityCardLi = document.createElement("li");
+        createdCityCardLi.classList.add("city");
+        createdCityCardLi.innerHTML = `
+        <h2 class="city-name" data-name="${name}, ${sys.country}">
+            <span>${name}</span>
+            <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+            <img class="city-icon" src="${iconUrl}">
+            <figcaption>${weather[0].description}</figcaption>
+        </figure>`;
+        cityList.prepend(createdCityCardLi);
+        // form.reset() ==> input.value = "";
+
+        form.reset();
+        input.focus();
+            
     } 
     catch (error) {
         msg.innerText = error;
+        setTimeout(()=>{
+            msg.innerText = "";
+        }, 5000);
     }
 
 }
-
-
 
 
 
